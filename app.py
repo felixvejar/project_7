88de491a382592e63aa96ad3b4d880d2b31035c5
felixvejar <felixvejarzepeda@yahoo.com>
2025-06-20 08:17:57 -0600

    Actualización

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.title("Mi primera app de análisis con Streamlit")
+st.header("Mi primera app web")
+car_data = pd.read_csvf("vehicles_us.csv")
+hist_button = st.button("Construir Histograma")
+scatter_button = st.button("Construir Gráfico de Dispersión")
 
-data = pd.DataFrame({
-    'Categoría': ['A', 'B', 'C'],
-    'Valor': [10, 30, 20]
-})
+if hist_button:
+    st.write(
+        "Creación de un histograma para el conjunto de datos de anuncions de venta de coches")
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
 
-fig = px.bar(data, x='Categoría', y='Valor', title='Gráfico de barras simple')
-st.plotly_chart(fig)
+if scatter_button:
+    st.write("Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches")
+    fig = px.scatter(car_data, x="odometer", y="price", color="manufacturer")
+    st.plotly_chart(fig, use_container_width=True)
